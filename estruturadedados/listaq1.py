b9363f08d5a0ac6c9ffd754c60aad5ea633af642
esteresttrela <ester_m_rodrigues@estudante.sesisenai.org.br>
2023-11-21 20:13:50 -0300

    list 2

--- estruturadedados/listaq1.py
+++ estruturadedados/listaq1.py
@@ -1,3 +1,14 @@
 nomes= ["Luan", "Gustavo", "Ester", "iago"]
 for nome in nomes:
     print(nome) 
+    #procurar um nome na lista
+if "vinicius" in nomes: 
+    print("aluno está presente")
+else:
+    print("Aluno faltou")
+
+    #concatenar outra lista
+    outros_nomes =  ["helder", "aduyar"]
+    todos_alunos = nomes + outros_nomes 
+    print(todos_alunos)
+                    
